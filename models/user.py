0620d5543acc9d67430a5553843878a4a00c05e8
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2025-11-18 19:41:35 -0300

    fix: User Payload

--- models/user.py
+++ models/user.py
@@ -7,7 +7,7 @@ from pydantic import BaseModel, Field
 
 class UserPayload(BaseModel):
     username: str = Field(..., description="Nome global do usuário")
-    member_id: str = Field(..., description="ID do usuário (Discord)")
+    member_id: Optional[str] = Field(None, description="ID do usuário (Discord)")
     full_name: Optional[str] = Field(None, description="Nome completo ou de exibição do usuário")
     email: Optional[str] = Field(None, description="Email do usuário")
     created_at: datetime = Field(default_factory=datetime.now, description="Data de criação do registro")
